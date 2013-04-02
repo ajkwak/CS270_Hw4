30be14167d881f3dd87e7c29f9a88dcc5fe8dc7e
ajkwak <aj@kwak.fake>
2013-04-02 10:34:31 -0700

    part_2a

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -5,10 +5,8 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
-    # flunk Movie.all
   end
   assert movies_table.hashes.size == Movie.all.count
-  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one

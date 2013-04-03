f0558ae1772afc3bd903c9e64531e1aca841d86c
ajkwak <aj@kwak.fake>
2013-04-02 17:20:01 -0700

    Hw3 Part 3: Implement tests on happy paths for sorting movies by title and by release date

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,8 +14,7 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.index(/#{e1}.*#{e2}/m).should_not == nil
 end
 
 # Make it easier to express checking or unchecking several boxes at once
